9b67d7e08cf4420eb3e83d60043d3687ae8544f6
YohanDecamps <yohan.decamps@epitech.eu>
2026-01-23 20:27:13 -0500

    feat: imersive viewport config, dynamic resize, orthographic camera

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,44 @@
-// src/main.ts
 import * as THREE from 'three'
+import './style.css'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+const camera = new THREE.OrthographicCamera(
+  window.innerWidth / -200,
+  window.innerWidth / 200,
+  window.innerHeight / 200,
+  window.innerHeight / -200,
   0.1,
-  100
+  1000
 )
 
 camera.position.z = 3
+camera.position.y = 3
+camera.position.x = 3
+
+camera.lookAt(0, 0, 0)
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 document.body.appendChild(renderer.domElement)
+renderer.domElement.style.display = 'block'
+
+window.addEventListener('resize', () => {
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  camera.left = width / -200
+  camera.right = width / 200
+  camera.top = height / 200
+  camera.bottom = height / -200
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshStandardMaterial({ color: 0xff5555 })
