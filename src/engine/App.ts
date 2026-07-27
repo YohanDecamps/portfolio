d5ceb9653477ddb34707b8477d7f6d8d681f24f9
YohanDecamps <yohan.decamps@epitech.eu>
2026-07-27 13:34:53 +0200

    feat: reposition name text

--- src/engine/App.ts
+++ src/engine/App.ts
@@ -89,7 +89,7 @@ export class App {
 
   private update(dt: number) {
     world.step()
-    this.debugRenderer.update()
+    //this.debugRenderer.update()
 
     for (const u of this.gameObjects) {
       u.update(dt)
