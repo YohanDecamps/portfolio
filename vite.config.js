942f13ba79f304531cbb1226614423a28556f0fb
YohanDecamps <yohan.decamps@epitech.eu>
2026-07-29 16:43:23 +0200

    feat: deployment

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: [
+            'three',
+            'three/examples/jsm/loaders/GLTFLoader'
+          ],
+        },
+      },
+    },
+  },
+})
